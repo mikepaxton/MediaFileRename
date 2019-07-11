c10e9202748d50879dc647d32034a8bafc53ebc9
Mike Paxton <mike.paxton@me.com>
2019-07-11 15:52:22 -0700

    initial

---
+++ RemovePeriodsFromFileNames.py
@@ -0,0 +1,15 @@
+import os
+
+mediaFolder = "/home/pi/tmp"
+for directname, directnames, files in os.walk(mediaFolder):
+    for f in files:
+        # Split the file into the filename and the extension, saving
+        # as separate variables
+        filename, ext = os.path.splitext(str(f))
+        if "." in filename:
+            # If a '.' is in the name, rename, appending the suffix
+            # to the new file
+            new_name = filename.replace(".", "")
+            os.rename(
+                os.path.join(directname, f),
+                os.path.join(directname, new_name + ext))
